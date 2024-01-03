60f9e230f5ee43647ca8761f788b937ac48fe2f8
KingOfZeHill <kingofzehill@gmail.com>
2024-01-03 09:55:18 +0100

    UPD ==> for test ISSUES - create.cshtml  :add "TEST SMO" label in <div asp-validation-summary="All" - create.fr/en.resx: add "TEST SMO" string in CreatProductTitle var of resource files

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,7 +1,19 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using Xunit;
+
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
+    // Arrange.
+    // private readonly IConfiguration _configuration;
+    // private readonly IStringLocalizer<ProductService> _localizer;
+
     public class ProductServiceTests
     {
         /// <summary>
@@ -22,5 +34,35 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         }
 
         // TODO write test methods to ensure a correct coverage of all possibilities
-    }
+
+        /* Code Help for adding test method
+         * 
+         * DB Connection :
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+                    var builder = new DbContextOptionsBuilder<AppIdentityDbContext>();
+                    var connectionString = configuration.GetConnectionString("P3Identity");
+                    builder.UseSqlServer(connectionString);
+                    return new AppIdentityDbContext(builder.Options, configuration);
+
+
+            builder.Services.AddDbContext<P3Referential>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("P3Referential")));
+
+            builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("P3Identity")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddDefaultTokenProviders();
+
+                */
+
+        [Fact]
+        public void SaveNewProduct()
+        {
+
+        }
 }
